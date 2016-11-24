27149c9ca6a4d310f670c28d60e9f3c8fa63591d
coderwurst <coderwurst@gmail.com>
2016-11-24 09:35:36 +0100

    reorganise folders

---
+++ myfirstangular2app/superceeded/metadata/metadata_app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+import {ItemComponent} from './item-list.component';
+
+@Component({
+    selector: 'my-app',
+    template: `<my-list></my-list>`,
+    directives:[ItemComponent]
+})
+export class MyTemplate {}
